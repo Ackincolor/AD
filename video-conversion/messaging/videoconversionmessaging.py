d8487224f1d2378d87aee1f7c750b06b8f913f1d
lois <lois.guillet@gmail.com>
2019-11-20 19:24:25 +0100

    pubsub in python

--- video-conversion/messaging/videoconversionmessaging.py
+++ video-conversion/messaging/videoconversionmessaging.py
@@ -16,16 +16,20 @@ logging.getLogger("pika").setLevel(logging.INFO)
 class VideoConversionMessaging(Thread):
     def __init__(self,_config_,converting_service):
         Thread.__init__(self)
-        self.subscriber = pubsub_v1.SubscriberClient()
-        self.subsciption_path = self.subscriber.subscription_path(_config_.get_project_id(),_config_.get_subscription_name())
-
+        self.converting_service = converting_service
+        subscriber = pubsub_v1.SubscriberClient()
+        subsciption_path = subscriber.subscription_path(_config_.get_project_id(),_config_.get_subscription_name())
+        subscrition_project = subscriber.project_path(_config_.get_project_id());
+        for subscription in subscriber.list_subscriptions(subscrition_project):
+            print(subscription)
         def callback(message):
-            print('Received message: {}'.format(message))
+            print('Received message: {}'.format(message.data.decode('utf-8')))
             message.ack()
+            self._on_message_(message)
 
-        self.subscriber.subscribe(self.subsciption_path, callback=callback)
+        subscriber.subscribe(subsciption_path, callback=callback)
 
-        print('listening fo message on {}'.format(self.subsciption_path))
+        print('listening fo message on {}'.format(subsciption_path))
         while True:
             time.sleep(60)
 #     def __init__(self, _config_, converting_service):
@@ -78,15 +82,14 @@ class VideoConversionMessaging(Thread):
 #         convert_request = json.loads(body.decode())
 #         logging.info(convert_request)
 #         self.converting_service.convert(convert_request["id"], convert_request['originPath'])
-#
-#     def _on_message_(self,  body):
-#         logging.info(body)
-#         # logging.info('id = %s, URI = %s', body["id"], body['originPath'])
-#         # logging.info('URI = %s', body['originPath'])
-#         logging.info('URI = %s', body.decode())
-#         convert_request = json.loads(body.decode())
-#         logging.info(convert_request)
-#         self.converting_service.convert(convert_request["id"], convert_request['originPath'])
+    def _on_message_(self,  body):
+         logging.info(body)
+         # logging.info('id = %s, URI = %s', body["id"], body['originPath'])
+         # logging.info('URI = %s', body['originPath'])
+         logging.info('URI = %s', body.data.decode())
+         convert_request = json.loads(body.data.decode())
+         logging.info(convert_request)
+         self.converting_service.convert(convert_request["id"], convert_request['originPath'])
 #
 #
 #
