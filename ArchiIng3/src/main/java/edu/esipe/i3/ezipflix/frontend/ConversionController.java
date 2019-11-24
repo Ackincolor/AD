b17c517c98f7bf72a2be943793f0a863bc91e3db
loisguillet <lois.guillet@gmail.com>
2019-11-24 14:46:59 +0100

    front page 2

---
+++ ArchiIng3/src/main/java/edu/esipe/i3/ezipflix/frontend/ConversionController.java
@@ -0,0 +1,16 @@
+package edu.esipe.i3.ezipflix.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ConversionController {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "index";
+    }
+
+}
